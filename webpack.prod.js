fd5ea67eefd21a6a6df2a007a2e1ed0c85209461
eya161 <araari.eya@gmail.com>
2024-04-25 07:54:39 +0100

    testing the devops part on backend

---
+++ webpack.prod.js
@@ -0,0 +1,12 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production', // or 'development'
+  entry: './server.js',
+  target: 'node', // Specify the target environment as Node.js
+  output: {
+    path: path.resolve(__dirname, 'dist'), // Output directory
+    filename: 'bundle.js', // Output bundle file name
+  },
+  // Other webpack configuration options
+};
